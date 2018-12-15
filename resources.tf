ff491609244b3ab30d229abf6f9d7b9fe1e10e87
fortem751 <fortem751@gmail.com>
2018-12-15 23:08:34 +0100

    modified resources

--- resources.tf
+++ resources.tf
@@ -18,4 +18,22 @@ resource "aws_subnet" "subnet1" {
   availability_zone = "us-east-1a"
 }
 
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.my_test_vpc.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.my_test_vpc.id}"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_security_group" "securitygroupsubnet" {
+  vpc_id = ${aws_vpc.my_test_vpc.id}
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.my_test_vpc.cidr_block}"
+    ]
+    from_port = 80
+    to_port =80
+    protocol = tcp
+  }
+}
 
