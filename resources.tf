a0b4e3568e8cc31a4480fa0f7878bad96ab7a701
fortem751 <fortem751@gmail.com>
2018-12-15 22:55:23 +0100

    file mode

--- resources.tf
+++ resources.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "my_test_vpc" {
 resource "aws_subnet" "subnet1" {
   cidr_block = "${cidrsubnet(aws_vpc.my_test_vpc.cidr_block, 3, 1)}"
   vpc_id = "${aws_vpc.my_test_vpc.id}"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 }
 
 
