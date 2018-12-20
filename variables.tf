415a2e0118613f49e5a5c37d848e296d6a516fe9
fortem751 <fortem751@gmail.com>
2018-12-20 10:43:08 +0100

    added variable file

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "s3_bucket_name" {
+  #default = "Super-Jo-s3-bucket"
+  description = "Name of the s3 bucket"
+  type = "string"
+}
+
+variable "s3_tags" {
+  type = "map"
+
+  default = {
+      created_by = "terraform"
+      environment = "Test"
+  }
+}
+
+variable "s3_regions" {
+  type = "list"
+  default = ["us-east-1", "eu-central-1"]
+}
+
+
